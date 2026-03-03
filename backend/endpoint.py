9a659b13b2963ae57750ee04108a4871f9dcc7fa
onkar kshirsagar <onkarkshirsagar297@gmail.com>
2026-03-03 12:12:45 +0530

    adding the file

--- backend/endpoint.py
+++ backend/endpoint.py
@@ -10,18 +10,20 @@ app = FastAPI(title="Stock Analysis Agent")
 
 
 origins = [
+    "https://stock-analysis-frontend-9nry.onrender.com",
     "https://stock-analysis-frontend-9nry.onrender.com", 
+    "https://stock-analysis-frontend.onrender.com",
     "http://localhost:5173",
-    "http://localhost:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 @app.post("/chat")
